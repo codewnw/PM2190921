c3f41ca1393944f1073c8a4a40136320e82a5a57
codewnw <code.wnw@gmail.com>
2020-01-05 11:10:32 +0530

    improved formatting

--- project_009_CBV/item/views.py
+++ project_009_CBV/item/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import (View, TemplateView, 
-ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.views.generic import (View, TemplateView,
+                                  ListView, DetailView, CreateView, UpdateView, DeleteView)
 from item.models import (Item)
 from django.urls import reverse_lazy
+
 
 def index(request):
     print('index method view')
     return render(request, 'index.html', {})
+
 
 class IndexView(View):
     def get(self, request):
         print('IndexView')
         return render(request, 'index.html', {})
 
+
 class AboutUsView(TemplateView):
     template_name = 'about_us.html'
+
 
 class ContactUsView(TemplateView):
     template_name = 'contact_us.html'
@@ -24,19 +28,24 @@ class ContactUsView(TemplateView):
         context['mobile'] = '9876543210'
         return context
 
+
 class ItemListView(ListView):
     model = Item
 
+
 class ItemDetailView(DetailView):
     model = Item
+
 
 class CreateItemView(CreateView):
     model = Item
     fields = ('name', 'description', 'price', 'image')
 
+
 class UpdateItemView(UpdateView):
     model = Item
     fields = ('name', 'description', 'price', 'image')
+
 
 class DeleteItemView(DeleteView):
     model = Item
