75138a0cefcb847fc8f48744bc9506b5948c81b1
codewnw <code.wnw@gmail.com>
2020-01-19 11:53:23 +0530

    job

--- vFour/jobs/urls.py
+++ vFour/jobs/urls.py
@@ -5,5 +5,9 @@ app_name = 'jobs'
 
 urlpatterns = [
     url(r'^$', views.JobList.as_view(), name='all'),
+    url(r'^list$', views.JobList.as_view(), name='all'),
     url(r'^(?P<pk>\d+)/', views.JobDetailView.as_view(), name='job_detail'),
+    url(r'^delete/(?P<pk>\d+)/', views.DeleteJobView.as_view(), name='job_delete'),
+    url(r'^update/(?P<pk>\d+)/', views.UpdateJobView.as_view(), name='job_update'),
+    url(r'^add$', views.CreateJobView.as_view(), name='add'),
 ]
